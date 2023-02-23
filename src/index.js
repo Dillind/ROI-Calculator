fa316cde38dc38c5b0d74b4686ee5c088b2b8e7d
Dylan <dylan@192-168-100-407.tpgi.com.au>
2023-02-23 16:44:55 +1100

    can display data collected from the Form onto the webpage.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,223 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
 import { Form, useFormAnswers } from "@quillforms/renderer-core";
 import "@quillforms/renderer-core/build-style/style.css";
 import { registerCoreBlocks } from "@quillforms/react-renderer-utils";
 registerCoreBlocks();
-
-
 import '../src/index.css'
 
+
 function App() {
-  // const formAnswers = useFormAnswers();
-  // console.log('answers', formAnswers)
+
+  // const [modal, setModal] = useState(false)
+
+  // displays the data from the user input
+  function displayData() {
+    return (
+      <div className='display-data'>
+        <p><b>Selected region:</b> {collectData["region"].value}</p>
+        <p>Role: {collectData["role"].value[0]}</p>
+        <p>What do you want to use Mastt for? {collectData["mastt-reason"].value}</p>
+        <p>How many projects do you have? {collectData["projects"].value}</p>
+        <p>How much value will be assigned to each project? {collectData["project-value"].value[0]}</p>
+        <p>How many staff do you have? {collectData["staff"].value}</p>
+      </div>
+    )
+  }
+
+  // collects data from form answers
+  const formAnswers = useFormAnswers()
+
+  const [collectData, setCollectData] = useState()
+
+  const [isInProgress, setIsInProgress] = useState(true)
+
+  // stores form data in the collectData state variable.
+  function storeData() {
+    setCollectData(formAnswers);
+    console.log(formAnswers)
+  }
+
+  return (
+  <div style={{ width: "100%", height: "100vh" }}>
+      <Form
+        formId="1"
+        formObj={{
+          blocks: [
+            // Welcome Screen
+            {
+              name: "welcome-screen",
+              id: "welcome",
+              attributes: {
+                label: "ROI Calculator",
+                description: "Calculate your ROI with Mastt.",
+                attachment: {
+                  type: "image",
+                  url:
+                    "https://quillforms.com/wp-content/uploads/2022/01/4207-ai-1.jpeg"
+                }
+              }
+            },
+            // Step 0: Get the region to know the internal $ rate for the region.
+            {
+              name: "dropdown",
+              id:"region",
+              attributes: {
+                required: true,
+                multiple: false,
+                label: "Please select your region.",
+                choices: [
+                  // by region or by country?
+                  { 
+                    label: "Americas",
+                    value: "Americas",
+                  },
+                  { 
+                    label: "Asia Pacific",
+                    value: "Asia Pacific",
+                  },
+                  { 
+                    label: "Europe",
+                    value: "Europe",
+                  },
+                  { 
+                    label: "Middle East",
+                    value: "Middle East",
+                  },
+                  { 
+                    label: "Africa",
+                    value: "Africa",
+                  },
+                ]
+              }
+            },
+            // // Step 1: Are you a consultant or Asset owner?
+            {
+              name: "multiple-choice",
+              id: "role",
+              attributes: {
+                required: true,
+                multiple: false,
+                verticalAlign: true,
+                label: "Are you a Consultant or Asset Owner?",
+                choices: [
+                  {
+                    label: "Consultant",
+                    value: "Consultant"
+                  },
+                  {
+                    label: "Asset Owner",
+                    value: "Asset Owner"
+                  },
+                ]
+              }
+            },
+            // // Step 2: What do you want to use Mastt for?
+            {
+              name: "multiple-choice",
+              id: "mastt-reason",
+              attributes: {
+                required: true,
+                multiple: false,
+                verticalAlign: true,
+                label: "What do you want to use Mastt for?",
+                choices: [
+                  {
+                    label: "Project",
+                    value: "Project"
+                  },
+                  {
+                    label: "Program",
+                    value: "Program"
+                  },
+                  {
+                    label: "Portfolio/Enterprise",
+                    value: "Portfolio/Enterprise"
+                  },
+                ]
+              }
+            },
+            // Step 3 - How many projects do you have?
+            {
+              name: "short-text",
+              id: "projects",
+              attributes: {
+                required: true,
+                label: "What is the total number of projects you have?",
+                setMaxCharacters: false,
+                maxCharacters: 0,
+              }
+            },
+            // Step 4 - Approximately, how much value will be attributed to each project?
+            {
+              name: "multiple-choice",
+              id: "project-value",
+              attributes: {
+                required: true,
+                multiple: false,
+                verticalAlign: true,
+                label: "How much value will be attributed to each project?",
+                // Is this enough choices? Does the price range work for the ROI calculations?
+                choices: [
+                  {
+                    label: "$0 - $9,999",
+                    value: "$0 - $9,999"
+                  },
+                  {
+                    label: "$10,000 - $49,999",
+                    value: "$10,000 - $49,999"
+                  },
+                  {
+                    label: "$50,000 - $99,999",
+                    value: "$50,000 - 99,999"
+                  },
+                  {
+                    label: "$100,000 +",
+                    value: "$100,000 +"
+                  },
+                ]
+              }
+            },
+
+            // // Step 5. How many staff do you currently have employed?
+            // // No. of Project Controllers
+
+            {
+              name: "short-text",
+              id: "staff",
+              attributes: {
+                required: true,
+                label: "How many staff members do you employ?",
+                setMaxCharacters: false,
+                maxCharacters: 0,
+              }
+            },
+          ],
+        }}
+        
+        onSubmit={(data, { completeForm, setIsSubmitting, goToBlock, setSubmissionErr},) => {
+          storeData()
+          setTimeout(() => {
+            setIsSubmitting(true);
+            completeForm();
+            setIsInProgress(false)
+          }, 500);
+        }}
+      />
+      <div>
+        {isInProgress ? "" : displayData()}
+      </div>
+      </div>
+  );
+}
+ 
+ReactDOM.render(
+React.createElement(App, {}, null),
+document.getElementById('react-target')
+);
+
+
 
   // const [investment, setInvestment] = useState()
   // const [gross, setGross] = useState()
@@ -41,232 +246,4 @@ function App() {
   // function grossReturned(e) {
   //   setGross(e.target.value)
   // }
-
-
-  // collect data from selected answers - push to a new collectData array?
-
-  function collectData() {
-    const formAnswers = useFormAnswers();
-    console.log(formAnswers)
-  }
-
-  console.log(collectData());
-
-  return (
-    // <div className='app-container'>
-    //   <div className='app-title'>
-    //     <h1>ROI CALCULATOR</h1>
-    //   </div>
-    //     <form className='form-container'>
-    //       <div className='invested-input'>
-    //         <span>Amount Invested</span>
-    //           <input
-    //           type="text"
-    //           placeholder=''
-    //           name='investment'
-    //           onChange={originalInvestment}
-    //           className="input"
-    //           />
-    //       </div>
-    //       <div className='returned-input'>
-    //         <span>Amount Returned</span>
-    //           <input
-    //           type="text"
-    //           placeholder=''
-    //           name='return'
-    //           onChange={grossReturned}
-    //           className="input"
-    //           />
-    //       </div>
-    //       <button className="calculate-btn"type="submit" onClick={onSubmit}>Calculate</button>
-    //     </form>
-    //     <div className='result-container'>
-    //       <h1>Result</h1>
-    //         <div className='statistics'>
-    //           <span>Investment Gain:</span>
-    //           <span>ROI:</span>
-    //           <span>Annualized ROI</span>
-    //         </div>
-    //     </div>
-
-        <div style={{ width: "100%", height: "100vh" }}>
-      <Form
-        formId="1"
-        formObj={{
-          blocks: [
-            {
-              name: "welcome-screen",
-              id: "jg1401r",
-              attributes: {
-                label: "ROI Calculator",
-                description: "Calculate your ROI with Mastt.",
-                attachment: {
-                  type: "image",
-                  url:
-                    "https://quillforms.com/wp-content/uploads/2022/01/4207-ai-1.jpeg"
-                }
-              }
-            },
-            // Step 0: Get the region to know the internal $ rate for the region.
-            {
-              name: "dropdown",
-              id:"q4ti234",
-              attributes: {
-                required: true,
-                multiple: false,
-                label: "Please select your region.",
-                choices: [
-                  // if (value === region) push internal $ rate for that region into an array?
-                  { 
-                    label: "Americas",
-                    value: "americas",
-                  },
-                  { 
-                    label: "Asia Pacific",
-                    value: "asia pacific",
-                  },
-                  { 
-                    label: "Europe",
-                    value: "europe",
-                  },
-                  { 
-                    label: "Middle East/Africa",
-                    value: "middle east/africa",
-                  },
-                ]
-              }
-            },
-            // Step 1: Are you a consultant or Asset owner?
-            {
-              name: "multiple-choice",
-              id: "kd12edg",
-              attributes: {
-                required: true,
-                multiple: false,
-                verticalAlign: true,
-                label: "Are you a Consultant or Asset Owner?",
-                choices: [
-                  {
-                    label: "Consultant",
-                    value: 'consultant'
-                  },
-                  {
-                    label: "Asset Owner",
-                    value: 'asset owner'
-                  },
-                ]
-              }
-            },
-            // Step 2: What do you want to use Mastt for?
-            {
-              name: "multiple-choice",
-              id: "t5fcv35",
-              attributes: {
-                required: true,
-                multiple: false,
-                verticalAlign: true,
-                label: "What do you want to use Mastt for?",
-                choices: [
-                  {
-                    label: "Project",
-                    value: "project"
-                  },
-                  {
-                    label: "Program",
-                    value: "program"
-                  },
-                  {
-                    label: "Portfolio/Enterprise",
-                    value: "portfolio/enterprise"
-                  },
-                ]
-              }
-            },
-            // Step 3 - How many projects do you have? - input field.
-
-            {
-              name: "short-text",
-              id: "masd24q",
-              attributes: {
-                required: true,
-                label: "What is the total number of projects you have?",
-                setMaxCharacters: true,
-                maxCharacters: 3,
-              }
-            },
-
-            // Step 4 - Approximately, how much value will be attributed to each project?
-            {
-              name: "multiple-choice",
-              id: "gq345da",
-              attributes: {
-                required: true,
-                multiple: false,
-                verticalAlign: true,
-                label: "What type of value will be attributed to each project?",
-                choices: [
-                  {
-                    label: "$0 - $9,999",
-                    value: "$0 - $9,999"
-                  },
-                  {
-                    label: "$10,000 - $49,999",
-                    value: "$10,000 - $49,999"
-                  },
-                  {
-                    label: "$50,000 - $99,999",
-                    value: "$50,000 - 99,999"
-                  },
-                  {
-                    label: "$100,000 +",
-                    value: "$100,000 +"
-                  },
-                ]
-              }
-            },
-
-            // Step 5. How many staff do you currently have employed?
-            // No. of Project Controllers
-
-            {
-              name: "short-text",
-              id: "p56t35q",
-              attributes: {
-                required: true,
-                label: "How many staff members do you employ?",
-                setMaxCharacters: true,
-                maxCharacters: 5,
-              }
-            },
-
-            // No. of Project Managers 
-            
-            // // Step 6. Present ROI Data / Pricing
-            // {
-              
-            // }
-          ],
-        }}
-        onSubmit={(data, { completeForm, setIsSubmitting, goToBlock, setSubmissionErr},) => {
-          console.log("Submitted!")
-          setTimeout(() => {
-            setIsSubmitting(false);
-            completeForm();
-          }, 500);
-        }}
-      />
-      </div>
-  );
-}
-
-ReactDOM.render(
-React.createElement(App, {}, null),
-document.getElementById('react-target')
-);
-
-// ReactDOM.createRoot(document.getElementById('react-target').render(
-// <React.StrictMode>
-//   <App/>
-// </React.StrictMode>
-// ))
 
